7d11f5ce503cec1022d44aa480c0bc2d09529852
BT_1N3_25@BKTECH.LOCAL <BT_1N3_25@BKTECH.LOCAL>
2018-09-27 08:34:28 -0400

    Initial commit

---
+++ src/FelixWilking/FelixWilkingLib.java
@@ -0,0 +1,8 @@
+package FelixWilking;
+
+public class FelixWilkingLib {
+    public static String test()
+    {
+        return("hi");
+    }
+}
---
+++ src/FelixWilking/Main.java
@@ -0,0 +1,9 @@
+package FelixWilking;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(FelixWilkingLib.test());
+    }
+}
